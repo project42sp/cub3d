b8a3c0ddd86c01744a61f567c878acfb0976e808
Solractys <carloseduardosilvasouzadelima@gmail.com>
2026-08-18 00:32:15 -0300

    fix: remove cmocka from cub3d header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: csilva-s <csilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/09 20:21:56 by csilva-s          #+#    #+#             */
-/*   Updated: 2026/08/09 20:21:58 by csilva-s         ###   ########.fr       */
+/*   Updated: 2026/08/18 00:31:52 by csilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 # include <stddef.h>
 # include <stdint.h>
 # include <setjmp.h>
-# include <cmocka.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
