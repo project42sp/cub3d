2814961200c7e4000d33547c58ef476c75559329
Solractys <carloseduardosilvasouzadelima@gmail.com>
2026-08-17 22:36:59 -0300

    feat: cmocka install and build script

--- src/test/cmocka.spec.c
+++ src/test/cmocka.spec.c
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cmocka.spec.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csilva-s <csilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/09 20:16:44 by csilva-s          #+#    #+#             */
-/*   Updated: 2026/08/09 20:21:44 by csilva-s         ###   ########.fr       */
+/*   Created: 2026/08/17 22:02:22 by csilva-s          #+#    #+#             */
+/*   Updated: 2026/08/17 22:02:23 by csilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/cub3d.h"
+#include <cmocka.h>
 
-int	main(void)
+static void  check_null_cases(void **states)
 {
-    write (1, "Windows 98 Screensaver\n", 23);
-    return (0);
+        (void)states;
+        int n = 2;
+        assert_false(n);
+}
+int main(void)
+{
+    const struct CMUnitTest tests[] = {cmocka_unit_test(check_null_cases),};
+    return (cmocka_run_group_tests(tests, NULL, NULL));
 }
