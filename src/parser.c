0a5f5fc9d377043c74b106f4a1d45fe4d54569bd
Bruna Uehara <uekabruna@gmail.com>
2026-09-14 00:09:23 -0300

    feat: init parser function + filename check function

--- src/parser.c
+++ src/parser.c
@@ -12,12 +12,32 @@
 
 # include "../includes/cub3d.h"
 
-int	name_check(char *filename)
+static int	invalid_name(char *filename)
 {
+	int		index;
+	char	**name_parts;
+	int		err;
 
+	name_parts = ft_split(filename, '.');
+	if (!name_parts)
+		return (FAILED);
+	index = 0;
+	while (name_parts[index + 1] != NULL)
+		index++;
+	err = SUCESS;
+	if (ft_strncmp(name_parts[index], "cub", 4))
+		err = FAILED;
+	ft_split_free(name_parts, index);
+	return (err);
 }
 
 t_scene	*parser(char *argv)
 {
-
+	if (invalid_name(argv))
+	{
+		ft_printf("%s\n", &"Error");
+		return (NULL);
+	}
+	write(1, &"TEST", 4);
+	return (NULL);
 }
