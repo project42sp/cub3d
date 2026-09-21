e305634dba83b01ef506bd57dd0448b57e067d64
Bruna Uehara <uekabruna@gmail.com>
2026-09-21 01:11:54 -0300

    fix: norminette

--- src/parser.c
+++ src/parser.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../includes/cub3d.h"
+#include "../includes/cub3d.h"
 
 int	invalid_name(char *filename)
 {
